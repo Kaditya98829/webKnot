887d83066c4be33c774391e5dc09f39b1e04cc41
kaditya98829 <kaditya98829@gmail.com>
2023-04-12 18:20:00 +0530

    Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'webknot';
+export class AppComponent implements OnInit{
+   public userData:any = [];
+   public userTitle:string='';
+  constructor(private userService: UserService){
+  }
+  ngOnInit(): void {
+    this.getData();
+  }
+  getData() {
+    this.userService.getUSerData().subscribe((res: any) => {
+      this.userData = res;
+      console.log(this.userData);
+    })
+  }
+ showTitle(event: any, title: string) {
+  this.userTitle = title;
+ }
 }
